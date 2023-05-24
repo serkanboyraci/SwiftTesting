3cdb9e4851542f49d829511f6340404d7334d08a
serkanboyraci <serkanboyraci@gmail.com>
2023-05-24 11:28:43 +0200

    added math func

---
+++ SwiftTesting/MathematicFunctions.swift
@@ -0,0 +1,24 @@
+//
+//  MathematicFunctions.swift
+//  SwiftTesting
+//
+//  Created by Ali serkan Boyracı  on 24.05.2023.
+//
+
+import Foundation
+
+class MathematicFunctions {
+    
+    func addIntegers( x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func multiplyIntegers(x: Int, y : Int) -> Int {
+        return x * y
+    }
+    
+    func divideIntegers(x: Int, y: Int) -> {
+        return x / y
+    }
+    
+}
