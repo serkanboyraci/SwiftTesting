6a107e2b1ec9e432e70474f74d32a0f3981a903e
serkanboyraci <serkanboyraci@gmail.com>
2023-05-24 12:21:11 +0200

    Unit test added

--- SwiftTestingTests/SwiftTestingTests.swift
+++ SwiftTestingTests/SwiftTestingTests.swift
@@ -9,28 +9,26 @@ import XCTest
 @testable import SwiftTesting
 
 final class SwiftTestingTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    let math = MathematicFunctions()
+    
+    func testAddIntegerFunction() {
+        let result = math.addIntegers(x: 5, y: 8)
+        
+        
+        XCTAssertEqual(result, 13)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testMultiplyIntegerFunction() {
+        let result = math.multiplyIntegers(x: 5, y: 8)
+        
+        XCTAssertEqual(result, 40)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testDivideIntegerFunction() {
+        let result = math.divideIntegers(x: 10, y: 2)
+        
+        XCTAssertEqual(result, 5)
     }
 
 }
